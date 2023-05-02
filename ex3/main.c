4156f447c4478b26362a58ac86396f5e47adf5bc
Guilherme <111463602+Guilherme-Arauj0@users.noreply.github.com>
2023-05-02 12:07:20 -0300

    Update main.c

--- ex3/main.c
+++ ex3/main.c
@@ -5,18 +5,16 @@ int main() {
    float vetor_quadrado[10];
    int i;
 
-   // Leitura dos valores do vetor original
+  
    printf("Digite 10 valores para preencher o vetor:\n");
    for (i = 0; i < 10; i++) {
       scanf("%f", &vetor_original[i]);
    }
 
-   // Cálculo do vetor com as componentes ao quadrado
    for (i = 0; i < 10; i++) {
       vetor_quadrado[i] = vetor_original[i] * vetor_original[i];
    }
 
-   // Impressão dos vetores original e ao quadrado
    printf("Vetor original: ");
    for (i = 0; i < 10; i++) {
       printf("%f ", vetor_original[i]);
